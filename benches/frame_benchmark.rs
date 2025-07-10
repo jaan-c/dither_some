99b98ad82ceb4324a334a2c8c998ea1aeaf37082
jaan-c <jaan-c@users.noreply.github.com>
2025-07-10 19:25:50 +0800

    Added benchmarking for frame.rs.

---
+++ benches/frame_benchmark.rs
@@ -0,0 +1,21 @@
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use dither_some::frame::Frame;
+
+fn frame_get_benchmark(c: &mut Criterion) {
+    let frame = Frame::new(1920, 1080);
+
+    c.bench_function("frame_get", |b| {
+        b.iter(|| frame.get_rgb(black_box(1), black_box(2)))
+    });
+}
+
+fn frame_set_benchmark(c: &mut Criterion) {
+    let mut frame = Frame::new(1920, 1080);
+
+    c.bench_function("frame_set", |b| {
+        b.iter(|| frame.set_rgb(black_box(1), black_box(2), black_box((1.0, 2.0, 3.0))))
+    });
+}
+
+criterion_group!(frame_benchmark, frame_get_benchmark, frame_set_benchmark);
+criterion_main!(frame_benchmark);
