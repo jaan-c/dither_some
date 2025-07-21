bcdff6b11020402fddfe7590145028b86e156d84
jaan-c <jaan-c@users.noreply.github.com>
2025-07-21 13:40:11 +0800

    Added benchmark for Floyd-Steinberg dithering.

---
+++ benches/dither_benchmark.rs
@@ -0,0 +1,39 @@
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use dither_some::{dither, frame::Frame};
+
+fn dither_atkinson_benchmark(c: &mut Criterion) {
+    let width = 1920;
+    let height = 1080;
+    let mut buf = vec![0u8; (width * height * 3) as usize];
+    let mut frame = Frame::new(width, height, &mut buf);
+
+    c.bench_function("dither_atkinson", |b| {
+        b.iter(|| {
+            dither::dither_frame_atkinson(black_box(&mut frame), black_box(2));
+        });
+    });
+}
+
+fn dither_floyd_steinberg_benchmark(c: &mut Criterion) {
+    let width = 1920;
+    let height = 1080;
+    let mut buf = vec![0u8; (width * height * 3) as usize];
+    let mut frame = Frame::new(width, height, &mut buf);
+
+    c.bench_function("dither_floyd_steinberg", |b| {
+        b.iter(|| {
+            dither::dither_frame_floyd_steinberg_color(black_box(&mut frame), black_box(2));
+        });
+    });
+}
+
+fn custom_criterion() -> Criterion {
+    Criterion::default().sample_size(20)
+}
+
+criterion_group! {
+    name = dither_benchmark;
+    config = custom_criterion();
+    targets = dither_atkinson_benchmark, dither_floyd_steinberg_benchmark
+}
+criterion_main!(dither_benchmark);
