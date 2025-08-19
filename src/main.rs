21e8fd7366037ca69963049105bcdbced1398ba3
jaan-c <jaan-c@users.noreply.github.com>
2025-08-20 06:14:54 +0800

    Handle temp file clean up more robustly.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, ValueEnum};
+use libc::{SIGINT, SIGTERM, c_int, signal};
 use std::fs;
 
 use crate::dither::{dither_frame_atkinson, dither_frame_floyd_steinberg_color};
@@ -33,24 +34,40 @@ enum Algorithm {
     FsColor,
 }
 
+extern "C" fn handle_signal(_sig: c_int) {}
+
 fn main() {
+    // Ignore these signals from the OS, instead handle ffmpeg shutdown as
+    // normal error so we have a chance to clean up.
+    unsafe {
+        signal(SIGINT, handle_signal as usize);
+        signal(SIGTERM, handle_signal as usize);
+    }
+
     let args = Args::parse();
     let temp_output = &format!("dither_some_{}", args.output);
 
-    ffmpeg::dither_frames_with(&args.input, &temp_output, |width, height, buffer| {
-        let (width, height) = (width as isize, height as isize);
+    if let Err(e) =
+        ffmpeg::dither_frames_with(&args.input, &temp_output, |width, height, buffer| {
+            let (width, height) = (width as isize, height as isize);
 
-        match args.algorithm {
-            Algorithm::Atkinson => dither_frame_atkinson(width, height, buffer, args.palette_count),
-            Algorithm::FsColor => {
-                dither_frame_floyd_steinberg_color(width, height, buffer, args.palette_count)
+            match args.algorithm {
+                Algorithm::Atkinson => {
+                    dither_frame_atkinson(width, height, buffer, args.palette_count)
+                }
+                Algorithm::FsColor => {
+                    dither_frame_floyd_steinberg_color(width, height, buffer, args.palette_count)
+                }
             }
-        }
-    })
-    .unwrap();
+        })
+    {
+        let _ = fs::remove_file(temp_output);
+        eprint!("{}", e);
+        return;
+    }
 
     let result =
         ffmpeg::copy_streams_or_aac_transcode_audio(&temp_output, &args.input, &args.output);
-    fs::remove_file(temp_output).ok();
+    let _ = fs::remove_file(temp_output);
     result.unwrap();
 }
